2f5dd4433b79e52fd1b3dc6bf7fdc65ab27056c3
Filip Golewski <f.golewski@gmail.com>
2018-01-24 12:40:21 +0100

    Message worker implementation

---
+++ OneManWebService/Worker/Implementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Web;
+
+namespace OneManWebService.Worker
+{
+    public class Implementation: IWork
+    {
+        public void Work(Context context)
+        {
+            while (true)
+            {
+                KeyValuePair<string, object> item = context.GetRequest();
+                if (item.Key == null)
+                    return;
+                try
+                {
+                    Thread.Sleep(10000);
+                    context.PutResponse(item.Key, item.Value);
+                }
+                catch
+                {
+                    context.PutRequest(item.Key, item.Value);
+                }
+            }
+        }
+    }
+}
